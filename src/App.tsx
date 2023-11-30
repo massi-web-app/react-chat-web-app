118a52761fd0eca7c58096c36d50d0eef120839b
masoud <masoud.moradi.developer@gmail.com>
2023-11-30 08:58:56 -0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+
         <Route path="/conversations" element={<ConversationPage />}>
           <Route path=":id" element={<ConversationChannelPage />} />
         </Route>
